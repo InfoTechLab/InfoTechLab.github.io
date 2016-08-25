1bc0769cd4157b3b030db0bcc13877843180e845
Jay <359390851@qq.com>
2016-08-25 11:07:16 +0800

    实验室主页

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            css: {
+                src: ['source/style/*.css'],
+                dest: 'temp/style/master.css'
+
+            },
+
+            dist: {
+                src: ['source/javascript/*.js', 'source/css/*.css'],
+                dest: 'temp/javascript/master.js',
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'public/style/master.min.css': ['temp/style/*.css']
+                }
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'source/javascript/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true,
+                    "boss": false,
+                    "curly": true,
+                    "eqeqeq": true,
+                    "expr": true,
+                    "immed": true,
+                    "newcap": true,
+                    "noempty": true,
+                    "noarg": true,
+                    "undef": true,
+                    "regexp": true,
+                    "browser": true,
+                    "devel": true,
+                    "node": true
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                src: ['temp/javascript/master.js'],
+                dest: "public/javascript/master.min.js"
+            }
+        },
+        copy: {
+            main: {
+                src: ['source/*.html', 'source/images/*', 'source/plug/*', 'source/style/*.eot', 'source/style/*.svg', 'source/style/*.ttf', 'source/style/*.woff'],
+                dest: 'public/',
+            }
+        },
+        clean: ["temp"]
+    });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-css');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('test', ['uglify']);
+    grunt.registerTask('cl', ['clean']);
+    grunt.registerTask('default', ['jshint', 'concat', 'copy', 'cssmin']);
+};
---
+++ source/javascript/index.js
@@ -0,0 +1,69 @@
+$(function () {
+    /*回到顶部*/
+    var $goTop = $(".go-top-gray").next(),
+        a = $(".header-menu").find("a"),
+        $division = $(".division"),
+        open = false,
+        num;
+    $(".go-top-gray").on("mouseover", function () {
+        $(this).animate({
+            opacity: 0
+        });
+        $goTop.animate({
+            opacity: 1
+        });
+    }).on("mouseout", function () {
+        $(this).animate({
+            opacity: 1
+        });
+        $goTop.animate({
+            opacity: 0
+        });
+    }).on("click", function () {
+        $(this).css("display", "none");
+        $("body,html").animate({
+            scrollTop: 0
+        });
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() !== 0) {
+            $(".wrap-go-top,.go-top-gray,.go-top").fadeIn();
+        } else {
+            $(".wrap-go-top,.go-top-gray,.go-top").fadeOut();
+        }
+    });
+    /*导航特效*/
+    $(a).each(function (i) {
+        if ($(this).parent().hasClass("active")) {
+            num=i;
+            $division.css({
+                marginLeft: (100 / a.length) * i + "%"
+            });
+        }
+           
+        $(this).on("mouseover", function () {
+            $division.stop().animate({
+                marginLeft: (100 / a.length) * i + "%"
+            }, 300);
+        }).on("mouseout", function () {
+            $division.stop().animate({
+                marginLeft: (100 / a.length) * num + "%"
+            }, 300);
+        });
+    });
+    $(".drop-menu-button").on("click", function () {
+        if (!open) {
+            $(this).find(".iconfont").css("color", "#53a1ab");
+            $(".drop-menu").animate({
+                height: "150px"
+            });
+            open = true;
+        } else {
+            $(this).find(".iconfont").css("color", "#60deed");
+            $(".drop-menu").animate({
+                height: 0
+            });
+            open = false;
+        }
+    });
+});
