1226b4fafbc4cc29c6bbd8103f65a6bcaf6c2f9b
Jay <359390851@qq.com>
2016-08-28 11:04:47 +0800

    加特效 duang duang duang

--- source/javascript/index.js
+++ source/javascript/index.js
@@ -66,15 +66,37 @@ $(function () {
             open = false;
         }
     });
-    var main1Top = $(".main-1 .content").offset().top;
-    var nowPosition;
+    var main1Left = $(".main-1 .left").offset().top,
+        main1Center = $(".main-1 .center").offset().top,
+        main1Right = $(".main-1 .right").offset().top,
+        main2Left = $(".main-2 .left").offset().top,
+        main2Center = $(".main-2 .center").offset().top,
+        main2Right = $(".main-2 .right").offset().top,
+        nowPosition = $(window).height() + $(document).scrollTop();
+    show(".main-1", main1Left, main1Center, main1Right, nowPosition);
+    show(".main-2", main2Left, main2Center, main2Right, nowPosition);
     $(document).on("scroll", function () {
         nowPosition = $(window).height() + $(document).scrollTop();
-        if (nowPosition >= main1Top) {
-            $(".main-1 .left,.main-1 .right").animate({
-                left: 0
-            },1500);
-            $(".main-1 .center").fadeIn("slow");
-        }
+        show(".main-1", main1Left, main1Center, main1Right, nowPosition);
+        show(".main-2", main2Left, main2Center, main2Right, nowPosition);
     });
 });
+
+function show(main, Left, Center, Right, nowPosition) {
+    if (nowPosition >= Left + 100) {
+        $(main + " .left").animate({
+            left: 0
+        }, 1500);
+    }
+    if (nowPosition >= Center + 100) {
+        $(main + " .center").animate({
+            left: 0,
+            opacity: 1
+        }, 1500);
+    }
+    if (nowPosition >= Right + 100) {
+        $(main + " .right").animate({
+            left: 0
+        }, 1500);
+    }
+}
