7156540e10c8b390e5082a3881d41bdd6c78fa71
Jay <359390851@qq.com>
2016-09-16 19:12:42 +0800

    微调

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function (grunt) {
                 dest: 'public/',
             }
         },
-        clean: ["temp", "source/source", "node_modules"]
+        clean: ["temp", "public/source", "node_modules"]
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
